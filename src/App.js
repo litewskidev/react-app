765b8a1b7ee01a5a85caefc814e457b5166d14b4
Adrian Litewski <litewskidev@gmail.com>
2023-01-12 23:54:42 +0100

    add basic structure & add sass

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Hero from "./components/Hero/Hero";
+import List from "./components/List/List";
+import SearchForm from "./components/SearchForm/SearchForm";
+
 const App = () => {
   return (
-    <h1>Hello world</h1>
+    <div>
+      <Hero />
+      <SearchForm />
+      <List />
+    </div>
   );
 };
 
-export default App;
+export default App;;
