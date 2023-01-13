903b13ab422e7007468569ebdf0975b71980fc9d
Adrian Litewski <litewskidev@gmail.com>
2023-01-13 01:56:13 +0100

    add container

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+import Container from "./components/Container/Container";
 import Hero from "./components/Hero/Hero";
 import List from "./components/List/List";
 import SearchForm from "./components/SearchForm/SearchForm";
 
 const App = () => {
   return (
-    <div>
+    <Container>
       <Hero />
       <SearchForm />
       <List />
-    </div>
+    </Container>
   );
 };
 
