5c205cc9f5d7d6a6f7a5d0b5f7671c4332c25d56
Adrian Litewski <litewskidev@gmail.com>
2023-01-13 04:49:29 +0100

    add Cards & CardForm

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,3 +1,5 @@
+import Card from '../Card/Card';
+import CardForm from '../CardForm/CardForm';
 import styles from './Column.module.scss';
 
 const Column = props => {
@@ -7,6 +9,10 @@ const Column = props => {
         <span className={styles.icon + ' fa fa-' + props.icon} />
         {props.title}
       </h2>
+      <ul className={styles.cards}>
+        {props.cards.map(card => <Card key={card.id} title={card.title} />)}
+      </ul>
+      <CardForm />
     </article>
   );
 };
