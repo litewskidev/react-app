d3ec8137b59e3c2fd0211f017c202a9935941c07
Adrian Litewski <litewskidev@gmail.com>
2023-01-13 01:28:55 +0100

    extract components

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,11 +1,13 @@
+import Button from '../Button/Button';
+import TextInput from '../TextInput/TextInput';
 import styles from './SearchForm.module.scss';
 
 const SearchForm = () => {
   return (
-      <form className={styles.SearchForm}>
-          <input className={styles.input} type="text" />
-          <button className={styles.button}>Search</button>
-      </form>
+    <form className={styles.SearchForm}>
+      <TextInput placeholder="Search..." />
+      <Button text="Search" />
+    </form>
   );
 };
 
