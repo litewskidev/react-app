608664e9389ff5a27b366d08ed8ca4eebb845415
Adrian Litewski <litewskidev@gmail.com>
2023-01-15 07:14:15 +0100

    add Home component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import Container from "./components/Container/Container";
-import Hero from "./components/Hero/Hero";
-import List from "./components/List/List";
+import Home from "./components/Home/Home";
 import NavBar from "./components/NavBar/NavBar";
-import SearchForm from "./components/SearchForm/SearchForm";
 
 const App = () => {
   return(
     <main>
       <NavBar />
       <Container>
-        <Hero />
-        <SearchForm />
-        <List />
+        <Home />
       </Container>
     </main>
   );
 };
 
-export default App;;
+export default App;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import Hero from "../Hero/Hero.js";
+import List from "../List/List.js";
+import SearchForm from "../SearchForm/SearchForm.js";
+
+const Home = () => {
+  return(
+    <>
+      <Hero />
+      <SearchForm />
+      <List />
+    </>
+  );
+};
+
+export default Home;
