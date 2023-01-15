079646a7b2cfde8ba66267d3d83ead9d95d82999
Adrian Litewski <litewskidev@gmail.com>
2023-01-15 03:15:21 +0100

    add action to reducer

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -1,16 +1,19 @@
 import styles from './ColumnForm.module.scss';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import Button from '../Button/Button';
 import TextInput from '../TextInput/TextInput';
 
-const ColumnForm = props => {
+const ColumnForm = () => {
+
+  const dispatch = useDispatch();
 
   const [title, setTitle] = useState('');
   const [icon, setIcon] = useState('');
 
   const handleSubmit = e => {
     e.preventDefault();
-    props.action({ title: title, icon: icon });
+    dispatch({ type: 'ADD_COLUMN', payload: { title, icon } });
     setTitle('');
     setIcon('');
   };
