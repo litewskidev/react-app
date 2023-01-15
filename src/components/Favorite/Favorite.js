fed5cffaf0aa6c1ce66d15c4df396bb8b97c0cbd
Adrian Litewski <litewskidev@gmail.com>
2023-01-15 06:56:35 +0100

    add new components

---
+++ src/components/Favorite/Favorite.js
@@ -0,0 +1,13 @@
+import PageTitle from '../PageTitle/PageTitle';
+import styles from './Favorite.module.scss';
+
+const Favorite = () => {
+  return(
+    <div>
+      <PageTitle>favorite</PageTitle>
+      <p className={styles.subtitle}>Lorem ipsum</p>
+    </div>
+  );
+};
+
+  export default Favorite;
