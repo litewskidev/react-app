eac4d3de1c2267849032ccbef7d7fec340b64f6d
Adrian Litewski <litewskidev@gmail.com>
2023-01-17 02:26:06 +0100

    add lists links

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import NavBar from "./components/NavBar/NavBar";
 import Container from "./components/Container/Container";
 import Home from "./components/Home/Home";
+import List from './components/List/List';
 import Favorite from "./components/Favorite/Favorite";
 import About from './components/About/About';
 import E404 from './components/404/E404';
@@ -13,6 +14,7 @@ const App = () => {
       <Container>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/list/:listId" element={<List />} />
           <Route path="/about" element={<About />} />
           <Route path="/favorite" element={<Favorite />} />
           <Route path='*' element={<E404 />} />
