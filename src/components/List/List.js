75a00d4a35996dc6ca2e0bb515bdd97cbce1d3f0
Adrian Litewski <litewskidev@gmail.com>
2023-01-17 02:48:03 +0100

    fix if no listID error

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,9 +3,8 @@ import styles from './List.module.scss'
 import ColumnForm from '../ColumnForm/ColumnForm';
 import { useSelector } from 'react-redux';
 import { getColumnsByList, getListById } from '../../redux/store';
-import { useParams } from 'react-router';
+import { Navigate, useParams } from 'react-router';
 import SearchForm from '../SearchForm/SearchForm';
-import { useState } from 'react';
 
 const List = () => {
 
@@ -14,21 +13,22 @@ const List = () => {
   const columns = useSelector((state) => getColumnsByList(state, listId));
   const listData = useSelector((state) => getListById(state, listId));
 
-  return(
-    <div className={styles.list}>
-      <header className={styles.header}>
-      <h2 className={styles.title}>{listData.title}</h2>
-      </header>
-      <p className={styles.description}>{listData.description}</p>
-      <SearchForm />
-      <section className={styles.columns}>
-        {columns.map(column =>
-        <Column key={column.id} {...column} />
-        )}
-      </section>
-      <ColumnForm listId={listId} />
-    </div>
-  );
+  if(!listData) return <Navigate to="/" />
+    return(
+      <div className={styles.list}>
+        <header className={styles.header}>
+        <h2 className={styles.title}>{listData.title}</h2>
+        </header>
+        <p className={styles.description}>{listData.description}</p>
+        <SearchForm />
+        <section className={styles.columns}>
+          {columns.map(column =>
+          <Column key={column.id} {...column} />
+          )}
+        </section>
+        <ColumnForm listId={listId} />
+      </div>
+    );
 };
 
 export default List;
