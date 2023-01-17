61d4a2247e3290668d89c2f4edd6e17341cfb9e7
Adrian Litewski <litewskidev@gmail.com>
2023-01-17 23:38:11 +0100

    style favorite module

--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -1,11 +1,24 @@
+import { useSelector } from 'react-redux';
+import { getFavoriteCard } from '../../redux/store';
+import Card from '../Card/Card';
 import PageTitle from '../PageTitle/PageTitle';
 import styles from './Favorite.module.scss';
 
 const Favorite = () => {
+
+  const cards = useSelector(getFavoriteCard);
+  if(!cards.length)
+        return (
+            <div className={styles.no_favorite_cards}>Sorry, no favorite cards!
+            </div>
+        );
+
   return(
     <div>
       <PageTitle>favorite</PageTitle>
-      <p className={styles.subtitle}>Lorem ipsum</p>
+      <ul className={styles.cards}>
+        {cards.map(card => <Card key={card.id} {...card}/>)}
+      </ul>
     </div>
   );
 };
